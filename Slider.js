aafba190cdee405036fc1dfc07bafd67a18a8ab5
DhiyaJoseph <149352526+DhiyaJoseph@users.noreply.github.com>
2023-10-30 10:56:53 +0530

    react website of light studio

---
+++ Slider.js
@@ -0,0 +1,10 @@
+
+function Slider() {
+    return (
+        <div className="container">
+            <img src='./slider.jpg' alt="main" className='main' />
+            <div className="centered">"If my images evoke memory or magic, then I have succeeded."</div>
+        </div>
+    )
+}
+export default Slider
