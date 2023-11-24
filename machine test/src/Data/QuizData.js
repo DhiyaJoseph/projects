942ee6cdb15be73489005f9599b71794ef29cd57
DhiyaJoseph <149352526+DhiyaJoseph@users.noreply.github.com>
2023-11-24 10:34:17 +0530

    QUIZ MASTER
    Dynamic Admin Panel using ReactJS
    Implemented features to add and manage multiple-choice questions,
    including answers, and assigned time limits for each question.
    User authentication and interactive testing

---
+++ machine test/src/Data/QuizData.js
@@ -0,0 +1,22 @@
+export const QuizData = [
+    {
+        question: "What is the capital city of France?",
+        options: ["Berlin", "London", "Paris", "Madrid"],
+        answer: 3
+    },
+    {
+        question: "Which planet is known as the 'Red Planet'?",
+        options: ["Mars", "Venus", "Jupiter", "Saturn"],
+        answer: 1
+    },
+    {
+        question: "Who wrote 'Romeo and Juliet'?",
+        options: ["Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"],
+        answer: 2
+    },
+    {
+        question: "In what year did the United States declare its independence?",
+        options: ["1776", "1789", "1800", "1812"],
+        answer: 1
+    },
+];
