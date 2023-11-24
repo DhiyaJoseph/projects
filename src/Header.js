65fbadf8e31f5d2c8c86af263d8bfe2755440a3f
DhiyaJoseph <149352526+DhiyaJoseph@users.noreply.github.com>
2023-11-24 10:52:40 +0530

    ReactJS Lightstudio site

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+return(
+<div className="head">
+
+    <img src="./light.png" alt="top" className="top"/>
+    </div>
+)}
+
+export default Header
---
+++ src/workData.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        id:1,
+        image:"./one.webp",
+        title:"VISIONS IN VINTAGE",
+        description:"Created for the Bockford House of Fashion, this shoot focused on the unfocused, with everything tinted in nostalgia.",
+        price:"INR 200",
+        available:true
+    },
+    {
+        id:2,
+        image:"./two.jpg",
+        title:"SUNSET SIRENS",
+        description:"Playing with gradients and atmosphere, this collection of stills was commissioned by the Fairhill ArtHaus for their inauguration.",
+        price:"CAD 100",
+        available:false
+    },
+    {
+        id:3,
+        image:"./three.webp",
+        title:"A THOUSAND WORDS",
+        description:"This project brings the written and visual arts together in storied images. It was created for the 10th anniversary of Bockford Independent Press.",
+        price:"AED 500",
+        available:true
+    }
+
+]
