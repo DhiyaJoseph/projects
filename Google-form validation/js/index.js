89a021a1730a0652ec62190d6986a408f35d7467
DhiyaJoseph <149352526+DhiyaJoseph@users.noreply.github.com>
2023-11-24 11:08:02 +0530

    Google form validation

---
+++ Google-form validation/js/index.js
@@ -0,0 +1,12 @@
+function dhiya()
+{
+    document.getElementById("myImg").src = "/images/pic1.jpg";
+}
+function liya()
+{
+    document.getElementById("myImg").src = "/images/pic2.jpg";
+}
+function jeffin()
+{
+    document.getElementById("myImg").src = "/images/pic3.jpg";
+}
