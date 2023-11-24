b7dd651284966cfbf812b2ebda6dbb36d7ecc9bf
DhiyaJoseph <149352526+DhiyaJoseph@users.noreply.github.com>
2023-11-24 10:54:18 +0530

    registeration form -reactJS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [formdata, setFormdata] = useState({
+    FirstName: '',
+    LastName: '',
+    Email: '',
+    Comments: '',
+    isGraduate: true,
+    Qualification: '',
+    gender:''
+  });
+  console.log(formdata)
+  function handleChange(event) {
+    const { name, type, checked, value } = event.target;
+    setFormdata(function (preFormData) {
+      return {
+        ...preFormData,
+        [name]: type === "checkbox" ? checked : value
+      }
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='form-div'>
+
+      <form>
+        <label>First Name :</label>
+        <input type="text" onChange={handleChange} name="FirstName" value={formdata.FirstName} />
+
+        <label>Last Name :</label>
+        <input type="text" onChange={handleChange} name="LastName" value={formdata.LastName} />
+
+        <label>Email :</label>
+        <input type="email" onChange={handleChange} name="Email" value={formdata.Email} />
+
+        <label>Comments :</label>
+        <textarea name="Comments" onChange={handleChange} value={formdata.Comments} />
+
+        <label>Is Graduate :</label>
+        <input type="checkbox" onChange={handleChange} name="isGraduate" checked={formdata.isGraduate} />
+
+        <label>Qualification:</label>
+        <select onChange={handleChange} name='qualification'>
+          <option value="">Select</option>
+          <option value="Btech">Btech</option>
+          <option value="BCA">BCA</option>
+          <option value="BBA">BBA</option>
+        </select>
+
+        <label>Gender: </label>
+        <label>Male
+          <input type="radio" name="gender" value="male" onChange={handleChange} checked={formdata.gender==="male"}
+          />
+        </label>
+        <label>female
+          <input type="radio" name="gender" value="female" onChange={handleChange} checked={formdata.gender==="female"}
+          />
+        </label>
+      </form>
+
     </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './Header';
-import Slider from './Slider';
-import Footer from './Footer';
-import workData from './workData';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const works = workData.map(function (work) {
-  return (
-    <Footer
-      {...work}
-    />
-  )
-})
 root.render(
   <React.StrictMode>
-    <Header />
-    <Slider />
-    <h1>Recent Work</h1>
-    <div className="footer">
-
-      <div className="images">
-        {works}
-      </div>
-    </div>
+    <App />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+reportWebVitals();
